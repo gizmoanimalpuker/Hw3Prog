a340d36dbaf57a4fad1b04ae7ebcab2b3fd6b8d7
Jaden Long <Jaden@MysticKiwi.com>
2023-04-23 19:41:28 -0700

    upd

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
 
-var origArray = [2, 3, 4];
+var origArray = [2, 3, 4]; 
 
-
-function writeOrigArray() {
-  document.getElementById("textbox1").value = origArray[0];
-  document.getElementById("textbox2").value = origArray[1];
-  document.getElementById("textbox3").value = origArray[2];
+function updateTextboxes(array) {
+  document.getElementById("textbox1").value = array[0];
+  document.getElementById("textbox2").value = array[1];
+  document.getElementById("textbox3").value = array[2];
 }
-
 
 function squareValues() {
-  var squareArray = [];
-  for (var i = 0; i < origArray.length; i++) {
-    squareArray[i] = origArray[i] * origArray[i];
-  }
-  document.getElementById("textbox1").value = squareArray[0];
-  document.getElementById("textbox2").value = squareArray[1];
-  document.getElementById("textbox3").value = squareArray[2];
+  var squareArray = origArray.map(function(x) {
+    return x * x;
+  });
+  updateTextboxes(squareArray);
 }
-
 
 document.getElementById("cubeButton").addEventListener("click", function() {
-  var cubeArray = [];
-  for (var i = 0; i < origArray.length; i++) {
-    cubeArray[i] = origArray[i] * origArray[i] * origArray[i];
-  }
-  document.getElementById("textbox1").value = cubeArray[0];
-  document.getElementById("textbox2").value = cubeArray[1];
-  document.getElementById("textbox3").value = cubeArray[2];
+  var cubeArray = origArray.map(function(x) {
+    return x * x * x;
+  });
+  updateTextboxes(cubeArray);
 });
 
-
 function fourthPower() {
-  var powerArray = [];
-  for (var i = 0; i < origArray.length; i++) {
-    powerArray[i] = origArray[i] * origArray[i] * origArray[i] * origArray[i];
-  }
-  document.getElementById("textbox1").value = powerArray[0];
-  document.getElementById("textbox2").value = powerArray[1];
-  document.getElementById("textbox3").value = powerArray[2];
+  var powerArray = origArray.map(function(x) {
+    return x * x * x * x;
+  });
+  updateTextboxes(powerArray);
 }
-
 
 document.getElementById("powerButton").addEventListener("click", fourthPower);
 
-
-window.addEventListener("load", writeOrigArray);
+document.addEventListener("DOMContentLoaded", function() {
+  updateTextboxes(origArray);
+});
